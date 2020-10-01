4107f07733cea5448d3aa68019662c855345fcac
kramrm <48371021+kramrm@users.noreply.github.com>
2020-10-01 11:51:18 -0700

    Add docstring to webhook method

--- webhook.py
+++ webhook.py
@@ -3,6 +3,15 @@ import json
 import os
 
 def post_webhook(message, timestamp, title='Status', color=0):
+    """Post webhook to Discord
+    Set an environment variable for 'WEBHOOK' to point to the URI for your channel
+    
+    Attributes
+        message (str): The message to put in the embed
+        timestamp (str): ISO 8601 timestamp of the event
+        title (str): Stats of the build process for the embed title.  Defaults to 'Status'
+        color (int): Color to use for embed highlight. Defaults to black
+    """
     url = os.environ.get('WEBHOOK')
     data = {}
     data['embeds'] = []
